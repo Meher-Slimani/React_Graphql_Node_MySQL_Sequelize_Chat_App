6d8865f8161f4913d0b0c152304b3d50b301b58a
Meher Slimani <meher.slimeni@gmail.com>
2021-03-28 23:52:20 +0100

    Reaction mutation & subscription

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,12 +1,22 @@
 const userResolvers = require("./users");
 const messageResolvers = require("./messages");
 
+const { Message, User } = require("../../models");
+
 module.exports = {
   Message: {
     createdAt: (parent) => parent.createdAt.toISOString(),
   },
   User: {
     createdAt: (parent) => parent.createdAt.toISOString(),
+  },
+  Reaction: {
+    createdAt: (parent) => parent.createdAt.toISOString(),
+    Message: async (parent) => await Message.findByPk(parent.messageId),
+    User: async (parent) =>
+      await User.findByPk(parent.userId, {
+        attributes: ["username", "imageUrl", "createdAt"],
+      }),
   },
   Query: {
     ...userResolvers.Query,
--- graphql/resolvers/messages.js
+++ graphql/resolvers/messages.js
@@ -1,11 +1,12 @@
 const {
   UserInputError,
   AuthenticationError,
+  ForbiddenError,
   withFilter,
 } = require("apollo-server");
 const { Op } = require("sequelize");
 
-const { User, Message } = require("../../models");
+const { User, Message, Reaction } = require("../../models");
 
 module.exports = {
   Query: {
@@ -65,6 +66,50 @@ module.exports = {
         throw err;
       }
     },
+    reactToMessage: async (_, { uuid, content }, { user, pubsub }) => {
+      const reactions = ["❤️", "😆", "😯", "😢", "😡", "👍", "👎"];
+      try {
+        // Validate reaction content
+        if (!reactions.includes(content))
+          throw new UserInputError("Invalid reaction");
+
+        // Get User
+        const username = user ? user.username : "";
+        user = await User.findOne({ where: { username } });
+        if (!user) throw new AuthenticationError("Unauthenticated");
+
+        // Get message
+        const message = await Message.findOne({ where: { uuid } });
+        if (!message) throw new UserInputError("Message not found");
+
+        if (message.from !== user.username && message.to !== user.username) {
+          throw new ForbiddenError("Unauthorized");
+        }
+
+        let reaction = await Reaction.findOne({
+          where: { messageId: message.id, userId: user.id },
+        });
+
+        if (reaction) {
+          // Reaction exists: update it
+          reaction.content = content;
+          await reaction.save();
+        } else {
+          // Reaction dosn't exist: create it
+          reaction = await Reaction.create({
+            messageId: message.id,
+            userId: user.id,
+            content,
+          });
+        }
+
+        pubsub.publish("NEW_REACTION", { newReaction: reaction });
+
+        return reaction;
+      } catch (err) {
+        throw err;
+      }
+    },
   },
   Subscription: {
     newMessage: {
@@ -72,13 +117,29 @@ module.exports = {
         (_, __, { pubsub, user }) => {
           if (!user) throw new AuthenticationError("Unauthenticated");
 
-          return pubsub.asyncIterator(["NEW_MESSAGE"]);
+          return pubsub.asyncIterator("NEW_MESSAGE");
         },
         ({ newMessage }, _, { user }) => {
           if (
             newMessage.from === user.username ||
             newMessage.to === user.username
           ) {
+            return true;
+          }
+          return false;
+        }
+      ),
+    },
+    newReaction: {
+      subscribe: withFilter(
+        (_, __, { pubsub, user }) => {
+          if (!user) throw new AuthenticationError("Unauthenticated");
+
+          return pubsub.asyncIterator("NEW_REACTION");
+        },
+        async ({ newReaction }, _, { user }) => {
+          const message = await newReaction.getMessage();
+          if (message.from === user.username || message.to === user.username) {
             return true;
           }
           return false;
